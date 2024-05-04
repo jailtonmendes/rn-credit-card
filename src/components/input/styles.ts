0ffd2e5e3b20648aca328b02266359cc4859925d
Jailton Mendes <jailtonmendes.silva@gmail.com>
2024-05-04 00:30:54 -0300

    finalizando interação dos campos com o cartão

---
+++ src/components/input/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    input: {
+        height: 54,
+        backgroundColor: "#E4ECF3",
+        padding: 16
+    }
+})
